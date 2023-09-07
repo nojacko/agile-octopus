400aa7f237f7728c4dd18a88c4289e33a1574b12
James Jackson <nojacko@gmail.com>
2023-09-07 01:34:03 +0100

    Postcode lookup

--- src/lib/RegionSelect.svelte
+++ src/lib/RegionSelect.svelte
@@ -3,26 +3,73 @@
     import { browser } from "$app/environment";
     import regions, { validRegion } from "./regions";
     export let region: string;
-
+    
     const defaultRegion = "B";
+    let showLookup = false;
+    let lookupLoading = false;
+    let lookupNotFound = false;
+    let lookupError = false;
+    let postcode = "";
+    let userRegion = "";
+    let userPostcode = "";
 
-    $: region, updatePriceCap();
+    $: region, updateRegion();
+    $: showLookup, resetLookup()
 
-    const updatePriceCap = function() {
+    const updateRegion = function() {
         if (validRegion(region)) {
             if (browser && localStorage) {
-                localStorage.setItem("priceCap", region);
+                localStorage.setItem("region", region);
             }
         } else {
             region = defaultRegion;
         }
     }
 
+    const resetLookup = function() {
+        lookupError = false;
+        lookupNotFound = false;
+    }
+
+    const lookupRegion = async function() {
+        if (postcode && postcode.length < 3 && postcode.length > 20) {
+            return;
+        }
+
+        resetLookup();
+
+        try {
+            lookupLoading = true;
+            userPostcode = postcode;
+            const lookupResp = await fetch(`https://api.octopus.energy/v1/industry/grid-supply-points/?postcode=${userPostcode}`);
+            const lookupJson = await lookupResp.json();
+
+            let suggestedRegion = "";
+            if (lookupJson && lookupJson.results.length === 1) {
+                suggestedRegion = lookupJson.results[0].group_id.replace(/[^a-z0-9]/gi, '');
+                if (validRegion(suggestedRegion)) {
+                    userRegion = suggestedRegion;
+                    region = suggestedRegion;
+                }
+            }
+
+            if (suggestedRegion === "") {
+                lookupNotFound = true;
+            }
+        } catch (err) { 
+            lookupError = true;
+        }
+        
+        setTimeout(() => {
+        lookupLoading = false;
+        }, 2*1000)
+    }
+
     onMount(() => {
         // Load region or set to default
         let localRegion = "";
         if (browser && localStorage) {
-            localRegion = localStorage.getItem("priceCap") || "";
+            localRegion = localStorage.getItem("region") || "";
         }
         region = (validRegion(localRegion)) ? localRegion : defaultRegion;
     });
@@ -36,7 +83,49 @@
             <option value={key}>{regions[key]} ({key})</option>
         {/each}
     </select>
+    <button on:click={() => { showLookup = !showLookup }} class="btn btn-outline-secondary" type="button">
+        <i class="fa-solid fa-circle-question"></i>
+    </button>
 </div>
+
+{#if showLookup}
+    <div class="card">
+        <div class="card-body">
+            <button type="button" class="btn btn-outline-secondary btn-sm float-end" on:click={() => { showLookup = false; }}><i class="fa-solid fa-xmark"></i></button>
+            
+            <h2 class="card-title">Lookup Your Region</h2>
+            <p>Enter your postcode to find your region.</p>
+
+            {#if lookupLoading} 
+                <div class="alert alert-secondary" role="alert">
+                    <i class="fa-solid fa-bolt fa-beat" style="color: Gold"></i> Loading...
+                </div>
+            {:else if userRegion.length && userPostcode === postcode} 
+                <div class="alert alert-secondary" role="alert">
+                    <i class="fa-solid fa-location-dot"></i> The region for <strong>{postcode.toUpperCase()}</strong> is <strong>{regions[userRegion]} ({userRegion})</strong>.
+                </div>
+            {:else if lookupError} 
+                <div class="alert alert-warning" role="alert">
+                    <i class="fa-solid fa-bug"></i> Something went wrong. Try again later.
+                </div>
+            {:else if lookupNotFound} 
+                <div class="alert alert-warning" role="alert">
+                    <i class="fa-regular fa-circle-question"></i> Could not find region for postcode. Please 
+                </div>
+            {/if}
+
+            <form on:submit|preventDefault={lookupRegion}>
+                <div class="input-group mb-3">
+                    <label class="input-group-text" for="region-select">Postcode</label>
+                    <input type="text" class="form-control" bind:value={postcode} maxlength="10">
+                    <button class="btn btn-outline-secondary" type="submit">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+{/if}
 
   
 
