36c714b0f64b500705df052051eb9134b4d9df44
James Jackson <nojacko@gmail.com>
2023-09-11 01:39:14 +0100

    More FAQs

--- src/lib/PriceCapInput.svelte
+++ src/lib/PriceCapInput.svelte
@@ -22,13 +22,6 @@
     });
 </script>
 
-<h2 id="price-cap">Price Cap</h2>
-<p>
-    The energy price cap is the maximum amount energy suppliers can charge you for each unit of energy if you're on a standard variable tariff.
-    Ofgrem's website has more information about the <a href="https://www.ofgem.gov.uk/information-consumers/energy-advice-households/energy-price-cap" target="_blank">energy price cap</a>.
-    You can customise the price cap value below.
-</p>
-
 <div class="input-group mb-4">
     <input bind:value={priceCap} type="number" min={min} max={max} class="form-control">
     <span class="input-group-text">pence/kWh</span>
@@ -36,4 +29,3 @@
         <i class="fa-solid fa-arrow-rotate-right"></i>
     </button>
 </div>
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,14 @@
     import { defaultPriceCap } from "$lib/price-cap";
     import type PricingHash from "$lib/PricesHash";
 
+	import IconAboveCap from "$lib/Icons/IconAboveCap.svelte";
 	import PriceCapInput from "$lib/PriceCapInput.svelte";
+	import PricingTableFooter from "$lib/PricingTableFooter.svelte";
+	import PricingWeekTable from "$lib/PricingWeekTable.svelte";
     import OctopusAd from "$lib/OctopusAd.svelte";
     import Price from "$lib/Price";
     import PricingTable from "$lib/PricingTable.svelte";
     import RegionSelect from "$lib/RegionSelect.svelte";
-	import PricingTableFooter from "$lib/PricingTableFooter.svelte";
-	import PricingWeekTable from "$lib/PricingWeekTable.svelte";
 
     const PRICE_TAB_UPCOMING = "upcoming";
     const PRICE_TAB_LAST_WEEK = "last-week";
@@ -150,6 +151,8 @@
 </div>
 
 <div class="container mb-4 mx-auto">
+    <OctopusAd />
+
     <h2 id="about">What Is Agile Octopus?</h2>
     <p>With Agile Octopus, you get access to half-hourly energy prices, tied to wholesale prices and updated daily. So when wholesale electricity prices drop, so do your bills - and if you can shift your daily electricity use outside of peak times, you can save even more.</p>
     <p>Agile Octopus includes Plunge Pricing that lets you take advantage of these negative price events, and get paid for the electricity you use!</p>
@@ -160,24 +163,43 @@
         <li>After the switch has completed, visit <a href="https://octopus.energy/smart/agile/" target="_blank">Agile Octpus</a> to sign up.</li>
     </ol>
 
-    <OctopusAd />
+
+    <h2>Cheapest Electricity?</h2>
+    <p>Generally, weekends are the cheapest. Daily, around <u>12am to 5am</u> is usually cheapest. Early afternoon around <u>12pm to 3pm</u> can be cheap too.</p>
+    <p>
+        <span class="text-warning-emphasis"><i class="fa-solid fa-triangle-exclamation"></i> Electricity prices can fluctuate!</span>
+        Anything from changes in natural gas global prices, power plant maintenance, weather conditions, and many others reasons.
+    </p>
+    <p>
+        <span class="text-info-emphasis"><i class="fa-solid fa-wind text-info-emphasis"></i> Check the weather!</span>
+        Wind power is the UK's largest renewable source so if it's going to be windy prices <em>should</em> drop.
+    </p>
+
+    <h2>Price Cap</h2>
+    <p>
+        The <u>{defaultPriceCap}p</u> electricity price cap is set by <a href="https://www.ofgem.gov.uk/information-consumers/energy-advice-households/energy-price-cap" target="_blank">Ofgem</a>.
+        <span class="text-warning-emphasis">Agile prices can go above the price cap!</span>
+        When this happens, you'll see <IconAboveCap />.
+        You can change the cap below.
+    </p>
     <PriceCapInput bind:priceCap={priceCap} defaultPriceCap={defaultPriceCap}></PriceCapInput>
+
+    <OctopusAd />
 
     <p class="fs-1 text-center mb-5">🐙</p>
 </div>
 
-
 <div id="nav-bar" class="container-fluid border-top border-secondary-subtle py-1 text-center text-body-secondary">
     <small>
-        <a href="#top" class="text-body-secondary"><i class="fa-solid fa-house"></i></a>
+        <a href="#top"><i class="fa-solid fa-house"></i></a>
         <i class="fa-solid fa-bolt fa-2xs"></i>
-        <a href="#region" class="text-body-secondary">Region</a>
+        <a href="#region">Region</a>
         <i class="fa-solid fa-bolt fa-2xs"></i>
-        <a href="#pricing-table" class="text-body-secondary">Pricing</a>
+        <a href="#pricing-table">Pricing</a>
         <i class="fa-solid fa-bolt fa-2xs"></i>
-        <a href="#about" class="text-body-secondary">FAQs</a>
+        <a href="#about">FAQs</a>
         <i class="fa-solid fa-bolt fa-2xs"></i>
-        <a href="#price-cap" class="text-body-secondary">Price Cap</a>
+        <a href="#join" style="color: Gold;">Free &pound;50!</a>
     </small>
 </div>
 
@@ -197,5 +219,8 @@
 
     #nav-bar a {
         color: White;
+    }
+    #nav-bar a.join {
+        color: Gold;
     }
 </style>
