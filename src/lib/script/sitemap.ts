50561597753f8b40e21cdc5dfd64eab2a4601a6b
James Jackson <nojacko@gmail.com>
2023-09-27 21:23:38 +0100

    Sitemap and script

---
+++ src/lib/script/sitemap.ts
@@ -0,0 +1,11 @@
+const BASE = "https://agile-octopus.datafury.io";
+const urls = [
+    "",
+    "about/",
+    "about/agile-octopus",
+    "about/plunge-pricing",
+];
+
+for (const url of urls) {
+    console.log(`${BASE}/${url}`);
+}
