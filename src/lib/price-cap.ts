99b85f1f6f3e7ff6441a4a4b1002aa101a451f60
James Jackson <nojacko@gmail.com>
2024-01-05 05:19:44 +0000

    New price cap

--- src/lib/price-cap.ts
+++ src/lib/price-cap.ts
@@ -1,8 +1,12 @@
-import { DateTime } from "luxon";
+// import { DateTime } from "luxon";
+//
+// // https://www.ofgem.gov.uk/energy-price-cap
+// let priceCap = 28.62;
+// if (DateTime.now() > DateTime.fromISO("2023-10-01T12:00:00Z").startOf('day')) {
+//     priceCap = 28.62; // New price
+// }
 
-const priceCapChange = DateTime.fromISO("2023-10-01T12:00:00Z").startOf('day');
-
-export const defaultPriceCap = (DateTime.now() > priceCapChange) ? 27 : 30;
+export const defaultPriceCap = 28.62;
 
 export const validPriceCap = function(value: number) {
     return (Number.isFinite(value) && value >= 0 && value <= 99)
