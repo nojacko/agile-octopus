60218c659a88fe1695f7f2cbcd77ca8241dfe8be
James Jackson <nojacko@gmail.com>
2024-01-05 05:25:29 +0000

    handleMissingId

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		prerender: {
+			handleMissingId: "ignore",
+		},
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// default options are shown. On some platforms
