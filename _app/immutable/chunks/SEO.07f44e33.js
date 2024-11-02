e0821e9eae05dd181dc05355bd693b6c791c96ba
James Jackson <nojacko@gmail.com>
2024-11-02 17:07:32 +0000

    Updates

---
+++ _app/immutable/chunks/SEO.07f44e33.js
@@ -0,0 +1,8 @@
+import{s as v,n as g}from"./scheduler.682ccc96.js";import{S as A,i as j,g as y,m as w,s as T,I as H,e as E,h as O,j as I,n as P,f as o,c as S,J as b,k as d,a as p,y as q,o as C}from"./index.a5adff1a.js";function D(s){let a,l=(s[0].length?`${s[0]} | ${_}`:_)+"",n,c,e,u,f,r,k=`<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "Agile Octopus Price Tracker",
+        "url": "https://agile-octopus.datafury.io/"
+    }
+<\/script>`,m;return{c(){a=y("title"),n=w(l),c=T(),e=y("meta"),f=T(),r=new H(!1),m=E(),this.h()},l(t){a=O(t,"TITLE",{});var i=I(a);n=P(i,l),i.forEach(o),c=S(t),e=O(t,"META",{name:!0,content:!0}),f=S(t),r=b(t,!1),m=E(),this.h()},h(){d(e,"name","description"),d(e,"content",u=s[1].length?`${s[1]} | ${h}`:h),r.a=m},m(t,i){p(t,a,i),q(a,n),p(t,c,i),p(t,e,i),p(t,f,i),r.m(k,t,i),p(t,m,i)},p(t,[i]){i&1&&l!==(l=(t[0].length?`${t[0]} | ${_}`:_)+"")&&C(n,l),i&2&&u!==(u=t[1].length?`${t[1]} | ${h}`:h)&&d(e,"content",u)},i:g,o:g,d(t){t&&(o(a),o(c),o(e),o(f),o(m),r.d())}}}const _="Agile Octopus Price Tracker",h="Quickly see the upcoming electricity prices for Octopus Energy's Agile Octopus tariff.";function J(s,a,l){let{title:n=""}=a,{description:c=""}=a;return s.$$set=e=>{"title"in e&&l(0,n=e.title),"description"in e&&l(1,c=e.description)},[n,c]}class Q extends A{constructor(a){super(),j(this,a,J,D,v,{title:0,description:1})}}export{Q as S};
