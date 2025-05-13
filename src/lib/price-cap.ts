13aec4ded48710bc1c89558889868842dc92cdd7
James Jackson <nojacko@gmail.com>
2025-05-13 17:13:46 +0100

    Price cap

--- src/lib/price-cap.ts
+++ src/lib/price-cap.ts
@@ -1,10 +1,10 @@
 import { DateTime } from "luxon";
 
 // https://www.ofgem.gov.uk/energy-price-cap
-let priceCap = 22.36;
-if (DateTime.now() > DateTime.fromISO("2024-10-01T12:00:00Z").startOf('day')) {
-    priceCap = 24.50; // New price
-}
+let priceCap = 27.03;
+// if (DateTime.now() > DateTime.fromISO("2024-10-01T12:00:00Z").startOf('day')) {
+//     priceCap = 24.50; // New price
+// }
 
 export const defaultPriceCap = priceCap;
 
