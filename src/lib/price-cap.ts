67c4d31f0257dfbeeff5a6c6b83962d7444729da
James Jackson <nojacko@gmail.com>
2025-05-25 03:09:33 +0100

    Updated price cap for 2025-07

--- src/lib/price-cap.ts
+++ src/lib/price-cap.ts
@@ -2,9 +2,9 @@ import { DateTime } from "luxon";
 
 // https://www.ofgem.gov.uk/energy-price-cap
 let priceCap = 27.03;
-// if (DateTime.now() > DateTime.fromISO("2024-10-01T12:00:00Z").startOf('day')) {
-//     priceCap = 24.50; // New price
-// }
+if (DateTime.now() >= DateTime.fromISO("2025-07-01T00:00:00Z").startOf('day')) {
+    priceCap = 25.73; // New price
+}
 
 export const defaultPriceCap = priceCap;
 
